f241fbf73a3585c5329610be332ae9e2aa6608f2
Gordon Zam <gordonzam@gmail.com>
2023-10-30 19:34:30 +1300

    responsiveness wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,64 @@
+'use client';
 import Image from 'next/image';
 import styles from './page.module.css';
-import Link from 'next/link';
 import './home.css';
+import { useState } from 'react';
 
 export default function Home() {
+  const [cardDetails, setCardDetails] = useState({
+    numbers: '0000 0000 0000 0000',
+    name: 'Jane Appleseed',
+    month: '00',
+    year: '00',
+    code: '000',
+  });
+  // const [cardNumbers, setCardNumbers] = useState('0000 0000 0000 0000');
+  // const [cardName, setCardName] = useState('Jane Appleseed');
+  // const [cardMonth, setCardMonth] = useState(0o00);
+  // const [cardYear, setCardYear] = useState(0o00);
+  // const [cardCode, setCardCode] = useState(00);
+
   return (
     <main className='cards-main'>
       <div className='cards-background'>
-        <Image
-          src={`/images/bg-card-back.png`}
-          alt='back card illustration'
-          width={0}
-          height={0}
-          sizes='100vw'
-          // style={{ width: '20vw', height: 'auto' }}
-          className='backCard'
-        />
-        <Image
-          src={`/images/bg-card-front.png`}
-          alt='front card illustration'
-          width={0}
-          height={0}
-          sizes='100vw'
-          // style={{ width: '20vw', height: 'auto' }}
-          className='frontCard'
-        />
-        <div className='card-numbers'></div>
-        <div className='card-name'></div>
-        <div className='card-date'></div>
-        <div className='card-cvc'></div>
+        <div className='cards-img-container'>
+          <div className='backCard-container'>
+            <Image
+              src={`/images/bg-card-back.png`}
+              alt='back card illustration'
+              width={0}
+              height={0}
+              sizes='100vw'
+              // style={{ width: '20vw', height: 'auto' }}
+              className='backCard'
+            />
+            <p className='card-cvc'>{cardDetails.code}</p>
+          </div>
+          <div className='frontCard-container'>
+            <Image
+              src={`/images/bg-card-front.png`}
+              alt='front card illustration'
+              width={0}
+              height={0}
+              sizes='100vw'
+              // style={{ width: '20vw', height: 'auto' }}
+              className='frontCard'
+            />
+            <Image
+              src={`/images/card-logo.svg`}
+              alt='card logo'
+              width={0}
+              height={0}
+              sizes='100vw'
+              className='cardLogo'
+            />
+            <p className='card-numbers'>{cardDetails.numbers}</p>
+            <p className='card-name'>{cardDetails.name}</p>
+            <p className='card-date'>
+              {cardDetails.month}/{cardDetails.year}
+            </p>
+          </div>
+        </div>
       </div>
       <section className='card-details'>
         <form className='card-form'>
